df0d271a5bcf0a9dbdfcd49d001fc2c240a7eac6
moonlitcharms <hnrstark@gmail.com>
2025-04-06 21:15:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '100px'}}>
+      <h1>Hello, Welcome to your New React App!</h1>
+    </div>
+  );
+}
+
+export default App;
